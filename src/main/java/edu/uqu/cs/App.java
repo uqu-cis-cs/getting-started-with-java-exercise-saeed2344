1d26df2eb3f956c56c1ba7cd7602919c3acd9335
saeed2344 <113013252+saeed2344@users.noreply.github.com>
2022-09-18 21:14:34 +0300

    Update App.java

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -15,6 +15,6 @@ public class App
 {
     public static void main ( String[] args )
     {
-        System.out.println( "Hello : Saeed Gazy Nasaldin 443000987" );
+        System.out.println( "Hello : Saeed Gazy Nasaldin , 443000987" );
     }
 }
